4d0103a8fb8a6d0ae6ba591e1df9076334f18f06
Elizabeth Karst <lkarst@outlook.com>
2018-08-18 21:14:13 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,7 +23,7 @@ end
 def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.each do |cheese|
-    snacks.include?(cheese)
+    if snacks.include?(cheese)
       return cheese
     else
       return nil
